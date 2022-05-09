7477fadcccfe5f874f58c7edb1f97557f7f009b8
FLilian <91611020+FLilian@users.noreply.github.com>
2022-05-09 11:39:38 +0200

    Update Lili_OS_14.c

--- GCNS8SBeadando/Lili_OS_14.c
+++ GCNS8SBeadando/Lili_OS_14.c
@@ -21,36 +21,36 @@
 
 int main(int argc, char *argv[])
 {
-    key_t key= 111; //shm kulcsa
+    key_t key= 111; 
     int shmid;
         char *guess_mem, *userinput;
-        if(( shmid = shmget(key,1024,0666|IPC_CREAT)) < 0 ){ //osztott memória kreálása (kulcs, méret, jogosultság | creálásmód)
-                perror("shmget"); // error kezelés
+        if(( shmid = shmget(key,1024,0666|IPC_CREAT)) < 0 ){
+                perror("shmget"); 
                 exit(1);
         }
 
     int f,pid;
     
 
-        f = open("myfifo", O_RDONLY); //fifo nyit
+        f = open("myfifo", O_RDONLY); 
         
-        read(f, &pid, sizeof(int)); //pid kiolvas
+        read(f, &pid, sizeof(int)); 
         close(f);
-        unlink("myfifo"); //bezár minden
+        unlink("myfifo"); 
         printf("read pid: %d \n", pid);
         printf("Adja meg az üzenetet: ");
     
     size_t len = 0;
     ssize_t lineSize = 0;
 
-    if ((guess_mem = shmat(shmid,NULL,0)) == (char *) -1){ //osztott mem hez csatlakozás
-                perror("shmat"); //error kezelés
+    if ((guess_mem = shmat(shmid,NULL,0)) == (char *) -1){ 
+                perror("shmat"); 
                 exit(1);
         }
-    lineSize = getline(&userinput, &len, stdin); //get user input
-    strcpy(guess_mem, userinput); // inputot az osztott memóriába másoljuk
-    shmdt(guess_mem); //lecsatlakozás az osztott mem
-    kill(pid, SIGUSR1); //signal küld
-    exit(0); //kilép
+    lineSize = getline(&userinput, &len, stdin); 
+    strcpy(guess_mem, userinput); 
+    shmdt(guess_mem); 
+    kill(pid, SIGUSR1); 
+    exit(0); 
     return 0;
 }
