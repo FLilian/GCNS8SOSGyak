720d228072304fcaffec1eb0e467014ae4cda0d4
FLilian <91611020+FLilian@users.noreply.github.com>
2022-05-09 02:32:48 +0200

    Create GCNS8Sfel.c

---
+++ GCNS8S0308/GCNS8Sfel.c
@@ -0,0 +1,26 @@
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+//pico 1_system.c
+//cc 1_system.c -o 1_system.out
+// ./1_system.out
+int main(void){
+    int status;
+
+    if( (status = system("date"))<0 )
+        perror("system() error");
+
+
+    if(WIFEXITED(status))
+        printf("Normalis befejezodes , visszaadott ertek = %d\n",WEXITSTATUS(status));
+
+    if( (status = system("GCNS8S"))<0 )
+		perror("system() errpr");
+
+    if(WIFEXITED(status))
+        printf("Normalis befejezodes , visszaadott ertek = %d\n",WEXITSTATUS(status));
+    exit(0);
+}
+
