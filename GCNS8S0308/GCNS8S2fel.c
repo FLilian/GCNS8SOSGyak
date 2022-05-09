61e4e670b6552f87eaf264ac201f35896f7db4ef
FLilian <91611020+FLilian@users.noreply.github.com>
2022-05-09 02:35:13 +0200

    Create GCNS8S2fel.c

---
+++ GCNS8S0308/GCNS8S2fel.c
@@ -0,0 +1,14 @@
+#include	<sys/types.h>
+#include	<sys/wait.h>
+#include	<stdio.h>
+#include 	<stdlib.h>
+#include 	<unistd.h>
+
+int main(void){
+	do{
+		printf(„Irj be egy parancsot!\n”);
+		char parancs[100];
+		scanf(„%s”,parancs);
+		execl(parancs);
+	}while(strcmp(parancs,”Q”)!=0);
+	exit(0);
